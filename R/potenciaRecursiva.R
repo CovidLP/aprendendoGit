51766d154f828d4a0239b0bd57d7ae2dc36b1183
Guido A. Moreira <guidoalber@gmail.com>
2021-07-26 18:38:29 +0100

    Added help for potenciaRecursiva

--- R/potenciaRecursiva.R
+++ R/potenciaRecursiva.R
@@ -1,3 +1,13 @@
+#' Function that returns the power of a number
+#'
+#' Return the power in the natural or log scale. Function recursion exercise
+#' @param x Base
+#' @param expo expoente em ingles
+#' @param logScale Whether the result should be returned in the log scale. If TRUE
+#' \code{x} must be provided in the log scale as well.
+#' @return The result of the calculations.
+#' @examples
+#' potenciaRecursiva(2, 6)
 #' @export
 potenciaRecursiva <- function(x, expo = 1, logScale = FALSE) {
   if (expo == 1) return(x)
